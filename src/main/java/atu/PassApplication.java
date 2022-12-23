a268da9327f67258131e19dad6529377940aef8c
Aidan Shields <g00370587@atu.ie>
2022-12-23 13:40:26 +0000

    test pipeline

--- src/main/java/atu/PassApplication.java
+++ src/main/java/atu/PassApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PassApplication {
-
+//test pipeline
 	public static void main(String[] args) {
 		SpringApplication.run(PassApplication.class, args);
 	}
